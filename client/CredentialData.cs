8e089c8a9297955485b92262a422cf8bdd9c3c0f
Damien Bayes <damien.bayes.db@gmail.com>
2018-05-02 19:35:50 +0600

    Create CredentialData.cs

---
+++ client/CredentialData.cs
@@ -0,0 +1,12 @@
+#region namespaces
+using System;
+#endregion
+
+namespace IPTS
+{
+    public class CredentialData
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
